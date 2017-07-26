4210a061af6ed406e5f543593c13a939ff9f2c39
Soma Holiday <soma@somaholiday.com>
2017-07-25 22:20:50 -0700

    update logparser with user_ids

--- logparser.js
+++ logparser.js
@@ -9,27 +9,81 @@ const patterns = {
 }
 
 const user_map = {
-  'James Troup': 'jamest',
-  'Nathan Alison': 'nfa',
-  'Arjun Baokar': 'arjun',
-  'Jason Ng': 'jng',
-  'Michael Millerick': 'mmillerick',
-  'Laney Erokan': 'laney',
-  'Yubo Su': 'yubo',
-  'Alex Fish': 'alexf',
-  'Gabriel Szczepanek': 'gabe',
-  'Charlotte Willens': 'charlotte',
-  'Danh Trang': 'danh',
-  'Rohan Varma': 'rohan',
-  'Bryan Lee': 'bryanl',
-  'Jeremy Lai': 'jeremy',
-  'Ryan Huttman': 'ryanh',
-  'Forest Trimble': 'forest',
-  'Alex Lew': 'alew',
-  'Eugene Marinelli': 'eugene',
-};
+  'Alex Fish': {
+    user_name: 'alexf',
+    user_id: 'U5H9W9WB0'
+  },
+  'Alex Lew': {
+    user_name: 'alew',
+    user_id: 'U21GVT7HB'
+  },
+  'Arjun Baokar': {
+    user_name: 'arjun',
+    user_id: 'U1X5009CH'
+  },
+  'Bryan Lee': {
+    user_name: 'bryanl',
+    user_id: 'U5H9WKAPQ'
+  },
+  'Charlotte Willens': {
+    user_name: 'charlotte',
+    user_id: 'U45GAP5AS'
+  },
+  'Forest Trimble': {
+    user_name: 'forest',
+    user_id: 'U263MEQTH'
+  },
+  'James Troup': {
+    user_name: 'jamest',
+    user_id: 'U5066LZGS'
+  },
+  'Jason Ng': {
+    user_name: 'jng',
+    user_id: 'U5GJF4KEH'
+  },
+  'Jeremy Lai': {
+    user_name: 'jeremy',
+    user_id: 'U1UUZB82V'
+  },
+  'Laney Erokan': {
+    user_name: 'laney',
+    user_id: 'U4PK0QSD6'
+  },
+  'Nathan Alison': {
+    user_name: 'nfa',
+    user_id: 'U1P0YT8H5'
+  },
+  'Danh Trang': {
+    user_name: 'danh',
+    user_id: 'U0BFCK27L'
+  },
+  'Eugene Marinelli': {
+    user_name: 'eugene',
+    user_id: 'U024JK9C9'
+  },
+  'Gabriel Szczepanek': {
+    user_name: 'gabe',
+    user_id: 'U66HB9336'
+  },
+  'Michael Millerick': {
+    user_name: 'mmillerick',
+    user_id: 'U4HNA59FX'
+  },
+  'Rohan Varma': {
+    user_name: 'rohan',
+    user_id: 'U5VBH8324'
+  },
+  'Ryan Huttman': {
+    user_name: 'ryanh',
+    user_id: 'U4QA86P34'
+  },
+  'Yubo Su': {
+    user_name: 'yubo',
+    user_id: 'U1P11BRE3'
+  }
+}
 
-const filename = 'word_ladder_log';
+const filename = 'word_ladders.slack';
 const output = 'word_ladders.json';
 
 const lines = fs.readFileSync(filename, 'utf-8').split('\n');
@@ -39,6 +93,7 @@ const lines = fs.readFileSync(filename, 'utf-8').split('\n');
 let year, month, day, hour, minute, ampm;
 let date;
 let user_name;
+let user_id;
 
 // CHANGE THESE TO MATCH BLEND
 const team_id = 'T060Z1Q6T';
@@ -58,12 +113,16 @@ _.each(lines, line => {
   }
 
   if (match = line.match(patterns.time)) {
-    user_name = user_map[_.trim(match[1])];
+    user_full_name = _.trim(match[1]);
+    const user = user_map[user_full_name];
+    user_name = user.user_name;
+    user_id = user.user_id;
+
     const timeString = match[2];
 
     const dateString = `${year} ${month} ${day} ${timeString}`;
     date = moment(dateString, 'YYYY MMMM Do h:mm A')
-    
+
     // console.log(dateString, date);
     // console.log(userName, timeString);
   }
@@ -76,6 +135,7 @@ _.each(lines, line => {
       channel_id,
       word,
       user_name,
+      user_id,
       text: line,
       timestamp: date.valueOf(),
     });
@@ -84,10 +144,10 @@ _.each(lines, line => {
 
 // console.log(entries);
 
-fs.writeFile(output, JSON.stringify(entries, null, 2), 'utf8', function (err) {
-    if (err) {
-        return console.log(err);
-    }
+fs.writeFile(output, JSON.stringify(entries, null, 2), 'utf8', function(err) {
+  if (err) {
+    return console.log(err);
+  }
 
-    console.log("The file was saved!");
-}); 
+  console.log("The file was saved!");
+});
