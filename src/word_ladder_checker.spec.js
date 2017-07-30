64dc6522a79dc8c53090660a46d1510018221be8
Soma Holiday <soma@somaholiday.com>
2017-07-29 19:38:45 -0700

    Clean up package.json, add tests for isValid

---
+++ src/word_ladder_checker.spec.js
@@ -0,0 +1,32 @@
+const _ = require('lodash');
+const checker = require('./word_ladder_checker');
+
+const test_cases = [
+  ['SILT', 'SALT', true],
+  ['ROOM', 'BOOM', true],
+
+  ['SLIDE', 'GLIDE', true],
+
+  ['WORD', 'RODE', false],
+
+  ['ROOM', 'BROOM', true],
+  ['BAND', 'BRAND', true],
+  ['HAND', 'HANDY', true],
+
+  ['BROOM', 'ROOM', true],
+  ['BRAND', 'BRAD', true],
+  ['HANDY', 'HAND', true],
+
+  ['SPON', 'SPOON', true],
+  ['HOOFS', 'HOOF', true],
+
+  ['BAD', 'BRAND', false],
+  ['FORTH', 'FOR', false],
+];
+
+_.each(test_cases, test_case => {
+  const [start, end, expected] = test_case;
+  test(`${start} → ${end} = ${expected}`, () => {
+    expect(checker.isValid(start, end)).toBe(expected);
+  });
+});
